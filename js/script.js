c88bece6147ce5bbaa8bc571ccb4804add7eef17
sergiykoshay <sergiy.koshay@gmail.com>
2016-12-08 19:56:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -62,10 +62,10 @@ $(function() {
 
 })( jQuery );
 function sender() {
-	tel = document.getElementById("user_phone").value;
-	next_st = "  _Номер телефону:_   +380";
-  	user_name = "_Ім'я:_ " + document.getElementById("user_name").value;
-  	user_message = "_Коментар:_ " + document.getElementById("user_message").value;
+	tel = $("user_phone").val();
+	next_st = "  _Номер телефону:_   ";
+  	user_name = "_Ім'я:_ " + $("user_name").val();
+  	user_message = "_Коментар:_ " + $("user_message").val();
   	message = user_name + next_st + tel + "   " + user_message;
    $.get("https://api.telegram.org/bot291742143:AAHSB2FvlPMwcTatGxhRB51RpKfyZzY5w84/sendMessage?parse_mode=markdown&text="+message+"&chat_id=-141035595"); //send data in telegram bot
     
