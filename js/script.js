32c495bfb2b441e423bf220d8444b7c4283a92f0
sergiykoshay <sergiy.koshay@gmail.com>
2016-12-08 20:53:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,67 +1,10 @@
-(function( $ ){
-	
-//// ---> Проверка на существование элемента на странице
-jQuery.fn.exists = function() {
-   return jQuery(this).length;
-}
 
-//	Phone Mask
-$(function() {
-  
-  if($('#user_phone').exists()){
+$(document).ready(function(){
     
-    $('#user_phone').each(function(){
-      $(this).mask("(999) 999-99-99");
-    });
+    $('#user_phone').mask("(99) 999-99-99");
     
-  }
-  
-  if($('.phone_form').exists()){
     
-    var form = $('.phone_form'),
-      btn = form.find('.btn_submit');
-    
-    form.find('.rfield').addClass('empty_field');
-  
-    setInterval(function(){
-    
-      if($('#user_phone').exists()){
-        var pmc = $('#user_phone');
-        if ( (pmc.val().indexOf("_") != -1) || pmc.val() == '' ) {
-          pmc.addClass('empty_field');
-        } else {
-            pmc.removeClass('empty_field');
-        }
-      }
-      
-      var sizeEmpty = form.find('.empty_field').size();
-      
-      if(sizeEmpty > 0){
-        if(btn.hasClass('disabled')){
-          return false
-        } else {
-          btn.addClass('disabled')
-        }
-      } else {
-        btn.removeClass('disabled')
-      }
-      
-    },200);
-
-    btn.click(function(){
-      if($(this).hasClass('disabled')){
-        return false
-      } else {
-        form.submit();
-      }
-    });
-    
-  }
-
-});
-
-})( jQuery );
-
+ 
 
 function sender() {
 	tel = $("#user_phone").val();
@@ -74,3 +17,4 @@ function sender() {
         $("#user_name").val("");  
 	$("#user_message").val("");
              }
+});
